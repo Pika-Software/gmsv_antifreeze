6081da975356fda34b6fb42eb5c116217dc21375
klen-list <anton.burkov.02@mail.ru>
2022-03-21 16:40:46 +0300

    upd lua

--- lua/autorun/server/antifreeze.lua
+++ lua/autorun/server/antifreeze.lua
@@ -1,3 +1,2 @@
-require("antifreeze")
-
---antifreeze.SetTimeout(30) --uncomment to manually set freeze kill timeout.
+require"antifreeze"
+--hang2kill.SetTimeout(30) --uncomment to manually set freeze kill timeout.

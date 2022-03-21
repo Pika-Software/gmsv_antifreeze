3d490135529f67f52493e208501e8927e117b46f
klen-list <anton.burkov.02@mail.ru>
2022-03-21 16:33:34 +0300

    workflows

---
+++ .github/workflows/test.lua
@@ -0,0 +1,13 @@
+timer.Simple(0, engine.CloseServer)
+
+assert(CI, 'tests expected to be running on configured dedicated server')
+
+local function run_tests()
+    require('hang2kill')
+end
+
+print('Running tests')
+run_tests()
+print('Tests are successful!')
+
+file.Write('success.txt', GITHUB_SHA)
